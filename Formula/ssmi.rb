cfbcafd399e28c109128c010a67a23e97fb75af9
Ido Avraham <idoavraham@idf.il>
2025-06-20 12:37:31 +0300

    bump ssmi to 1.0.4

--- Formula/ssmi.rb
+++ Formula/ssmi.rb
@@ -1,26 +1,26 @@
 class Ssmi < Formula
   desc "SSMI"
   homepage "https://github.com/idoavrah/ssmi"
-  version "1.0.3"
+  version "1.0.4"
   desc "Terminal-based sidekick for AWS SSM"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/idoavrah/ssmi/releases/download/#{version}/ssmi-#{version}-mac-silicon.tar.gz"
-      sha256 "a0b3787eb3f3e59555831b070b16e04beb30b993ccabd088ae4e00f5a29dada7"
+      sha256 "38cecdc898f683c5f0f26c0a160a50b73d7e3f54030e3494a26ffa36778f698d"
     else
       url "https://github.com/idoavrah/ssmi/releases/download/#{version}/ssmi-#{version}-mac-intel.tar.gz"
-      sha256 "2a7a5639c320d3df23c943110c6167bfc9f878da6fbb3e6b346d84379efe121c"
+      sha256 "8e1d9400a6954380653c6bf28e88ce247ab4a8dcf480692837fffd895e38c5ef"
     end
   end
   
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/idoavrah/ssmi/releases/download/#{version}/ssmi-#{version}-linux-arm64"
-      sha256 "23fba5d0c2c8adcc71e539f4b37a6f6f88786308d2ed3a171d10b62d092aae9d"
+      sha256 "8e0e3c9609c1424f2763590e952817080382da61c490d9e9ac252f0be37db0de"
     else
       url "https://github.com/idoavrah/ssmi/releases/download/#{version}/ssmi-#{version}-linux-amd64"
-      sha256 "9a249815a8b49ff5f62276f60405abc914bc739ccc24304798d550b1e3579dca"
+      sha256 "e3c60f28817a2d2fdc6c7780dca6b505c5eafad28deac8739160e7996873c596"
     end
   end
 
