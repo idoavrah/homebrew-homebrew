8819c0c8d7e10901769c56555cfcd53d56bd260f
Ido Avraham <idoavraham@idf.il>
2025-06-20 13:20:13 +0300

    update sha256 checksums for macOS and Linux binaries in ssmi formula

--- Formula/ssmi.rb
+++ Formula/ssmi.rb
@@ -7,20 +7,20 @@ class Ssmi < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/idoavrah/ssmi/releases/download/#{version}/ssmi-#{version}-mac-silicon.tar.gz"
-      sha256 "38cecdc898f683c5f0f26c0a160a50b73d7e3f54030e3494a26ffa36778f698d"
+      sha256 "504a65ac28fbdfa283ee904595e9f3c44b15bece621b29303822277c8cce6f5f"
     else
       url "https://github.com/idoavrah/ssmi/releases/download/#{version}/ssmi-#{version}-mac-intel.tar.gz"
-      sha256 "8e1d9400a6954380653c6bf28e88ce247ab4a8dcf480692837fffd895e38c5ef"
+      sha256 "d0be0d3d1443cf84f2801a54e6e602bfc95bf585014e74aac343922afa0f8be2"
     end
   end
   
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/idoavrah/ssmi/releases/download/#{version}/ssmi-#{version}-linux-arm64"
-      sha256 "8e0e3c9609c1424f2763590e952817080382da61c490d9e9ac252f0be37db0de"
+      sha256 "591c327e631ecba5f8af98fc700e27d6f5f7d4efc8663bbb1491cfe708304acd"
     else
       url "https://github.com/idoavrah/ssmi/releases/download/#{version}/ssmi-#{version}-linux-amd64"
-      sha256 "e3c60f28817a2d2fdc6c7780dca6b505c5eafad28deac8739160e7996873c596"
+      sha256 "030bcda398927d1bb9ce1abe0c9727cfc6dfef0db4695612c6ac90b5d0f0cfec"
     end
   end
 
